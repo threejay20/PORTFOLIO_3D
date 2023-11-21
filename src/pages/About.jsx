6f297c55a9e2f0eafe1af98231e5d6a1aeccfa55
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-11-21 22:08:06 +0800

    updated about section

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -12,14 +12,12 @@ const About = () => {
       <h1 className='head-text'>Hello, I'm <span className='blue-gradient_text font-semibold drop-shadow'>Justin</span></h1>
 
       <div className='mt-5 flex flex-col gap-3 text-slate-500'>
-        <p>A Leader with over 15 years of experience in call center environments and a strong background in business administration, 
-          management, and operations as well a Full Stack Engineer. I have completed multiple certifications in project management foundations and microservices architecture,
-           demonstrating my commitment to continuous learning and professional growth. As a Full Stack Engineer, I direct and manage the development and maintenance of full-stack web applications 
-           using HTML, JavaScript, CSS, and Java, ensuring high-quality code delivery and user satisfaction. 
-           I collaborate with cross-functional teams in designing and implementing efficient, scalable software solutions, fostering a collaborative work environment 
-           that drives innovation and excellence. Additionally, I leverage data analytics to optimize call center performance, reduce response time, increase productivity, 
-           and enhance customer experience. I also design and implement comprehensive sales training and coaching programs that boost cross-sell and retention rates, revenue, and profitability. 
-           I am passionate about creating value for my clients, partners, and colleagues, and I am always open to new challenges and opportunities.</p>
+        <p>Experienced leader with 15+ years in call center environments, 
+          specializing in business administration, management, and operations. 
+          Certified in project management and microservices architecture. 
+          Skilled Full Stack Engineer, proficient in directing web application development for high-quality code and user satisfaction. 
+          Collaborates cross-functionally to design scalable software solutions and optimize call center performance using data analytics. 
+          Expert in creating sales training programs to boost cross-sell and retention rates. Committed to delivering value and open to new challenges.</p>
       </div>
 
       <div className='py-10 flex flex-col'> 
