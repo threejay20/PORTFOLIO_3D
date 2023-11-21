e37053f842e6fd7089b4dd209985c4d876ee659a
threejay20 <118399217+threejay20@users.noreply.github.com>
2023-11-21 18:22:23 +0800

    added music as well as the button on home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import {Suspense, useState} from 'react'
+import {Suspense, useState, useEffect, useRef} from 'react'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
@@ -6,15 +6,27 @@ import Sky from '../models/Sky'
 import Bird from '../models/Bird'
 import Plane from '../models/Plane'
 import HomeInfo from '../components/HomeInfo'
-{/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center"> 
-      POPUP
 
-      </div> */}
+import sakura from '../assets/sakura.mp3'
+import { soundoff, soundon } from '../assets/icons'
 
 
 const Home = () => {
+  const audioRef = useRef(new Audio(sakura));
+  audioRef.current.volume = 0.4;
+  audioRef.current.loop = true;
   const [isRotating, setIsRotating ] = useState(false);
   const [currentStage, setCurrentStage] = useState(1)
+  const [isPlayingMusic, setIsplayingMusic] = useState(false);
+
+  useEffect(() => {
+if(isPlayingMusic) {
+  audioRef.current.play();
+}
+return () => {
+  audioRef.current.pause();
+}
+  }, [isPlayingMusic])
 
   const adjustIslandForScreenSize = () => {
     let screenScale = null;
@@ -84,6 +96,14 @@ const Home = () => {
       </Suspense>
 
      </Canvas>
+     <div className='absolute bottom-2 left-2'>
+      <img
+       src={!isPlayingMusic ? soundoff : soundon}
+       alt="sound"
+       className='w-10 h-10 cursor-pointer object-contain'
+       onClick={() => setIsplayingMusic(!isPlayingMusic)}
+       />
+     </div>
 
     </section>
   )
